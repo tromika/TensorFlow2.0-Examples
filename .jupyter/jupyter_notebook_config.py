befff50d693a36142d70d35f097c70f4d48491f0
Tamas Szuromi <tamas@codesandbox.io>
2023-05-10 09:25:19 +0000

    Update 3 files

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -2,4 +2,4 @@ c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.allow_origin = '*'
 c.NotebookApp.open_browser = False
 c.NotebookApp.allow_remote_access = True
-c.NotebookApp.port = 8889
+c.NotebookApp.port = 8888
